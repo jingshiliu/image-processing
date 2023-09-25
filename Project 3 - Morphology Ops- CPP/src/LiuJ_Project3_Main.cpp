d805140bef0bdf0bc47bd3fd654f1e7912e69514
Jingshi Liu <jingshiliu07@gmail.com>
2023-09-25 13:46:44 -0400

    chore(Morphology| Util): add overloaded "printArray" for 1D array

--- Project 3 - Morphology Ops- CPP/src/LiuJ_Project3_Main.cpp
+++ Project 3 - Morphology Ops- CPP/src/LiuJ_Project3_Main.cpp
@@ -35,6 +35,13 @@ namespace Util{
         }
         cout<<endl<<endl;
     }
+
+    static void printArray(int* array, int length){
+        for(int i = 0; i < length; i++){
+            cout<< array[i] << " ";
+        }
+        cout<<endl<<endl;
+    }
 }
 
 class Morphology{
