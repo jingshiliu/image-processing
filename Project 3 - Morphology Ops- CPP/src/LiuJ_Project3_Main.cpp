d716b954073328be7c0d8922761badf1a6c93b81
Jingshi Liu <jingshiliu07@gmail.com>
2023-09-24 22:11:55 -0400

    chore(Morphology| Util): create a namespace for Util, which contains methods "getArray, printArray"

--- Project 3 - Morphology Ops- CPP/src/LiuJ_Project3_Main.cpp
+++ Project 3 - Morphology Ops- CPP/src/LiuJ_Project3_Main.cpp
@@ -6,6 +6,37 @@
 
 using namespace std;
 
+namespace Util{
+    static int** getArray(int rows, int cols){
+        int** array = new int*[rows];
+        for(int i = 0; i < rows; i++){
+            array[i] = new int[cols];
+            for(int j = 0; j < cols; j++){
+                array[i][j] = 0;
+            }
+        }
+        return array;
+    }
+
+    static int* getArray(int length){
+        int* array = new int[length];
+        for(int i = 0; i < length; i++){
+            array[i] = 0;
+        }
+        return array;
+    }
+
+    static void printArray(int** array, int row, int col){
+        for(int i = 0; i < row; i++){
+            for(int j = 0; j < col; j++){
+                cout<<array[i][j]<<" ";
+            }
+            cout<<endl;
+        }
+        cout<<endl<<endl;
+    }
+}
+
 class Morphology{
 public:
     int numImageRows,
