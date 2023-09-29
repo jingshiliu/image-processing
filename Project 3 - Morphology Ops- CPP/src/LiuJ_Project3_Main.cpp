15914bba2da5a7fd5c9f04b9401a00fc631ca59e
Jingshi Liu <jingshiliu@yichendeAir-2.lan>
2023-09-28 22:35:15 -0400

    chore(Morphology): remove unused until functions

--- Project 3 - Morphology Ops- CPP/src/LiuJ_Project3_Main.cpp
+++ Project 3 - Morphology Ops- CPP/src/LiuJ_Project3_Main.cpp
@@ -18,62 +18,6 @@ namespace Util{
         return array;
     }
 
-    static int* getArray(int length){
-        int* array = new int[length];
-        for(int i = 0; i < length; i++){
-            array[i] = 0;
-        }
-        return array;
-    }
-
-    static void printArray(int** array, int row, int col){
-        for(int i = 0; i < row; i++){
-            for(int j = 0; j < col; j++){
-                cout<<array[i][j]<<" ";
-            }
-            cout<<endl;
-        }
-        cout<<endl<<endl;
-    }
-
-    static void printArray(int* array, int length){
-        for(int i = 0; i < length; i++){
-            cout<< array[i] << " ";
-        }
-        cout<<endl<<endl;
-    }
-
-    static void prettyPrint(int** array, int rows, int cols){
-        for(int i = 0; i < rows; i++){
-            for(int j = 0; j < cols; j++){
-                if(array[i][j] == 0){
-                    cout<< ". ";
-                }else{
-                    cout <<"1 ";
-                }
-            }
-            cout << "\n";
-        }
-    }
-
-    static void loadFileToArray(ifstream& inFile, int** array, int rows, int cols){
-        int pixelVal;
-        for(int i = 0; i < rows; i++){
-            for(int j = 0; j < cols; j++){
-                inFile >> pixelVal;
-                array[i][j] = pixelVal;
-            }
-        }
-    }
-
-    static void loadFileToArray(ifstream& inFile, int* array, int length){
-        int pixelVal;
-        for(int i = 0; i < length; i++){
-            inFile >> pixelVal;
-            array[i] = pixelVal;
-        }
-    }
-
     static int** copyArray(int** array, int rows, int cols){
         int** output = getArray(rows, cols);
         for(int i = 0; i < rows; i++){
