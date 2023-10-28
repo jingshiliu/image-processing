fae43b6e03070845d150d5f97d96e19c2dcf3604
Jingshi Liu <jingshiliu07@gmail.com>
2023-10-28 02:27:22 -0400

    chore(ImageCompression): implement DistanceTransform8

---
+++ Project 5 - Image Compression/src/LiuJ_Project5_Main.cpp
@@ -0,0 +1,267 @@
+//
+//  main.cpp
+//  CS381_Image_Processing_Project5_Distance_Transform
+//
+//  Created by Jingshi Liu on Oct/28/2023.
+//
+
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+namespace Util{
+    static int** getArray(int rows, int cols){
+        int** array = new int*[rows];
+        for(int i = 0; i < rows; i++){
+            array[i] = new int[cols];
+            for(int j = 0; j < cols; j++){
+                array[i][j] = 0;
+            }
+        }
+        return array;
+    }
+
+    static int* getArray(int length){
+        int* array = new int[length];
+        for(int i = 0; i < length; i++){
+            array[i] = 0;
+        }
+        return array;
+    }
+
+    static int findMin(int* array, int length){
+        int min = array[0];
+        for(int i = 1; i < length; i++){
+            if(array[i] < min){
+                min = array[i];
+            }
+        }
+        return min;
+    }
+
+    static int findMinSkipZero(int* array, int length){
+        int min = array[0];
+        for(int i = 1; i < length; i++){
+            if(array[i] < min && array[i] != 0){
+                min = array[i];
+            }
+        }
+        return min;
+    }
+
+    static int findMax(int* array, int length){
+        int max = array[0];
+        for(int i = 1; i < length; i++){
+            if(array[i] > max){
+                max = array[i];
+            }
+        }
+        return max;
+    }
+
+    static int findMax(int** array, int rows, int cols){
+        int max = array[0][0];
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < cols; j++){
+                if(array[i][j] > max){
+                    max = array[i][j];
+                }
+            }
+        }
+        return max;
+    }
+
+    static void printArray(int** array, int row, int col){
+        for(int i = 0; i < row; i++){
+            for(int j = 0; j < col; j++){
+                cout<<array[i][j]<<" ";
+            }
+            cout<<endl;
+        }
+        cout<<endl<<endl;
+    }
+
+    static void printArray(int* array, int length){
+        for(int i = 0; i < length; i++){
+            cout<< array[i] << " ";
+        }
+        cout<<endl<<endl;
+    }
+
+    static void prettyPrint(int** array, int rows, int cols){
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < cols; j++){
+                if(array[i][j] == 0){
+                    cout<< ". ";
+                }else{
+                    cout <<"1 ";
+                }
+            }
+            cout << "\n";
+        }
+    }
+
+    static void loadFileToArray(ifstream& inFile, int** array, int rows, int cols){
+        int pixelVal;
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < cols; j++){
+                inFile >> pixelVal;
+                array[i][j] = pixelVal;
+            }
+        }
+    }
+
+    static void loadFileToArray(ifstream& inFile, int* array, int length){
+        int pixelVal;
+        for(int i = 0; i < length; i++){
+            inFile >> pixelVal;
+            array[i] = pixelVal;
+        }
+    }
+
+    static int** copyArray(int** array, int rows, int cols){
+        int** output = getArray(rows, cols);
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < cols; j++){
+                output[i][j] = array[i][j];
+            }
+        }
+        return output;
+    }
+}
+
+class ImageCompression{
+public:
+    int numRows,
+        numCols,
+        minVal,
+        maxVal,
+        newMinVal,
+        newMaxVal;
+    int** ZFAry;
+    int** skeletonAry;
+
+    ImageCompression(ifstream& inFile){
+        inFile >> numRows >> numCols >> minVal >> maxVal;
+        ZFAry = Util::getArray(numRows + 2 , numCols + 2);
+        skeletonAry = Util::getArray(numRows + 2, numCols + 2);
+
+        loadImage(inFile);
+    }
+
+    void setZero(int** array){
+        for(int i = 0; i < numRows + 2; i++){
+            for(int j = 0; j < numCols + 2; j++){
+                array[i][j] = 0;
+            }
+        }
+    }
+
+    void loadImage(ifstream& inFile){
+        int pixelVal;
+        for(int i = 1; i < numRows + 1; i++){
+            for(int j = 1; j < numCols + 1; j++){
+                inFile >> pixelVal;
+                ZFAry[i][j] = pixelVal;
+            }
+        }
+    }
+
+    void imageReformat(int** image, ofstream& outFile){
+        outFile << numRows << " " << numCols << " " << minVal << " " << maxVal << '\n';
+        string str;
+        int curWidth,
+            pixelWidth = to_string(Util::findMax(image, numRows+2, numCols+2)).length();
+
+        for(int r = 1; r < numRows + 1; r++){
+            for(int c = 1; c < numCols + 1; c++){
+                outFile << image[r][c];
+                str = to_string(image[r][c]);
+                curWidth = str.length();
+                while(curWidth < pixelWidth){
+                    outFile<<' ';
+                    curWidth++;
+                }
+                outFile<<' ';
+            }
+            outFile << '\n';
+        }
+    }
+
+    void loadSkeleton(ifstream& inFile){
+        int pixelVal;
+        for(int i = 1; i < numRows + 1; i++){
+            for(int j = 1; j < numCols + 1; j++){
+                inFile >> pixelVal;
+                skeletonAry[i][j] = pixelVal;
+            }
+        }
+    }
+
+    void distance8(ofstream& outFile, ofstream& debugFile){
+        debugFile << "Entering distance8() method" << endl;
+        distance8Pass1(debugFile);
+        imageReformat(ZFAry, outFile);
+        distance8Pass2(debugFile);
+        imageReformat(ZFAry, outFile);
+        debugFile << "Exiting distance8() method" << endl;
+    }
+
+    void distance8Pass1(ofstream& debugFile){
+        debugFile << "Entering distancePass1() method" << endl;
+        for(int i = 1; i < numRows + 1; i++){
+            for(int j = 1; j < numCols + 1; j++){
+                if (ZFAry[i][j] == 0) continue;
+                int neighbors[4] = {ZFAry[i - 1][j - 1],
+                                  ZFAry[i - 1][j],
+                                  ZFAry[i - 1][j + 1],
+                                  ZFAry[i][j - 1]};
+                ZFAry[i][j] = 1 + Util::findMinSkipZero(neighbors, 4);
+            }
+        }
+        debugFile << "Exiting distancePass1() method" << endl;
+    }
+
+    void distance8Pass2(ofstream& debugFile){
+        debugFile << "Entering distancePass2() method" << endl;
+        for(int i = numRows; i > 0; i--){
+            for(int j = numCols; j > 0; j--){
+                if (ZFAry[i][j] == 0) continue;
+                int neighborsAndSelf[5] = {ZFAry[i][j + 1] + 1,
+                                    ZFAry[i + 1][j - 1] + 1,
+                                    ZFAry[i + 1][j] + 1,
+                                    ZFAry[i + 1][j + 1] + 1,
+                                    ZFAry[i][j]};
+                ZFAry[i][j] = Util::findMinSkipZero(neighborsAndSelf, 5);
+            }
+        }
+        debugFile << "Exiting distancePass2() method" << endl;
+    }
+};
+
+
+
+
+
+
+// --------------------------------------- Main Function ---------------------------------------------------------//
+
+int main(int argc, const char* argv[]){
+    ifstream inFile(argv[1]);
+    ofstream outFile(argv[2]),
+             debugFile(argv[3]),
+             skeletonFile((string)argv[1] + "_skeleton.txt"),
+             decompressedFile((string)argv[1] + "_decompressed.txt");
+
+    ImageCompression* imageCompression = new ImageCompression(inFile);
+    imageCompression->distance8(outFile, debugFile);
+
+
+    inFile.close();
+    outFile.close();
+    debugFile.close();
+    skeletonFile.close();
+    decompressedFile.close();
+    return 0;
+}
