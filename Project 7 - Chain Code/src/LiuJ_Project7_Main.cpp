d82413b45b4789f51a40dc0daeed739dbacda3be
Jingshi Liu <jingshiliu07@gmail.com>
2023-11-22 00:56:05 -0500

    chore(ChainCode): implement boundary reconstruction

--- Project 7 - Chain Code/src/LiuJ_Project7_Main.cpp
+++ Project 7 - Chain Code/src/LiuJ_Project7_Main.cpp
@@ -15,134 +15,6 @@ namespace Util{
         }
         return array;
     }
-
-    static int* getArray(int length){
-        int* array = new int[length];
-        for(int i = 0; i < length; i++){
-            array[i] = 0;
-        }
-        return array;
-    }
-
-    static int min(int a, int b){
-        return a < b ? a : b;
-    }
-
-    static int max(int a, int b){
-        return a > b ? a : b;
-    }
-
-    static int findMin(int* array, int length){
-        int min = array[0];
-        for(int i = 0; i < length; i++){
-            if(array[i] < min){
-                min = array[i];
-            }
-        }
-        return min;
-    }
-
-    static int findMin(int** array, int rows, int cols){
-        int min = array[0][0];
-        for(int i = 0; i < rows; i++){
-            for(int j = 0; j < cols; j++){
-                if(array[i][j] < min){
-                    min = array[i][j];
-                }
-            }
-        }
-        return min;
-    }
-
-    static int findMinSkipZero(int* array, int length){
-        int min = array[0];
-        for(int i = 0; i < length; i++){
-            if(array[i] < min && array[i] != 0){
-                min = array[i];
-            }
-        }
-        return min;
-    }
-
-    static int findMax(int* array, int length){
-        int max = array[0];
-        for(int i = 0; i < length; i++){
-            if(array[i] > max){
-                max = array[i];
-            }
-        }
-        return max;
-    }
-
-    static int findMax(int** array, int rows, int cols){
-        int max = array[0][0];
-        for(int i = 0; i < rows; i++){
-            for(int j = 0; j < cols; j++){
-                if(array[i][j] > max){
-                    max = array[i][j];
-                }
-            }
-        }
-        return max;
-    }
-
-    static void printArray(int** array, int row, int col){
-        for(int i = 0; i < row; i++){
-            for(int j = 0; j < col; j++){
-                cout<<array[i][j]<<" ";
-            }
-            cout<<endl;
-        }
-        cout<<endl<<endl;
-    }
-
-    static void printArray(int* array, int length){
-        for(int i = 0; i < length; i++){
-            cout<< array[i] << " ";
-        }
-        cout<<endl<<endl;
-    }
-
-    static void prettyPrint(int** array, int rows, int cols){
-        for(int i = 0; i < rows; i++){
-            for(int j = 0; j < cols; j++){
-                if(array[i][j] == 0){
-                    cout<< ". ";
-                }else{
-                    cout <<"1 ";
-                }
-            }
-            cout << "\n";
-        }
-    }
-
-    static void loadFileToArray(ifstream& inFile, int** array, int rows, int cols){
-        int pixelVal;
-        for(int i = 0; i < rows; i++){
-            for(int j = 0; j < cols; j++){
-                inFile >> pixelVal;
-                array[i][j] = pixelVal;
-            }
-        }
-    }
-
-    static void loadFileToArray(ifstream& inFile, int* array, int length){
-        int pixelVal;
-        for(int i = 0; i < length; i++){
-            inFile >> pixelVal;
-            array[i] = pixelVal;
-        }
-    }
-
-    static int** copyArray(int** array, int rows, int cols){
-        int** output = getArray(rows, cols);
-        for(int i = 0; i < rows; i++){
-            for(int j = 0; j < cols; j++){
-                output[i][j] = array[i][j];
-            }
-        }
-        return output;
-    }
 }
 
 class Point{
@@ -312,6 +184,21 @@ public:
         return chainDirection;
     }
 
+    void constructBoundary(ifstream& chainCodeFile){
+        int label, row, col, chainCode;
+        // pass the headers: numRows, numCols, minVal, maxVal
+        for(int i = 0; i < 4; i++){
+            chainCodeFile >> label;
+        }
+        chainCodeFile >> label >> row >> col;
+        reconstructAry[row][col] = label;
+        while(chainCodeFile >> chainCode){
+            row += coordOffset[chainCode].row;
+            col += coordOffset[chainCode].col;
+            reconstructAry[row][col] = label;
+        }
+    }
+
     ~ChainCode(){
         for(int i = 0; i < numRows + 2; i++){
             delete[] zeroFramedAry[i];
@@ -332,7 +219,17 @@ int main(int argc, const char* args[]){
 
     ChainCode chainCode(inFile);
     debugFile<< "After loadImage, zeroFramedAry: \n";
-    chainCode.reformatPrettyPrint(chainCode.zeroFramedAry, debugFile);
+    chainCode.reformatPrettyPrint(chainCode.zeroFramedAry, outFile);
+
+    chainCode.getChainCode(chainCodeFile, debugFile);
+    debugFile<< "After getChainCode, zeroFramedAry: \n";
+    chainCode.reformatPrettyPrint(chainCode.zeroFramedAry, outFile);
+
+    chainCodeFile.close();
+    ifstream chainCodeFileIn(args[1] + string("_chainCode.txt"));
+    chainCode.constructBoundary(chainCodeFileIn);
+    chainCode.reformatPrettyPrint(chainCode.reconstructAry, outFile);
+    chainCode.imageReformat(chainCode.reconstructAry, boundaryFile);
 
     return 0;
 }
