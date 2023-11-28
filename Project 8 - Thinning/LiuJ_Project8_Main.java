3dfa754196208783b7de9227e4358e71d3e4c429
Jingshi Liu <jingshiliu07@gmail.com>
2023-11-28 16:20:57 -0500

    chore(Thinning): create project folder

---
+++ Project 8 - Thinning/LiuJ_Project8_Main.java
@@ -0,0 +1,7 @@
+public class LiuJ_Project8_Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(new FileReader(args[0]));
+        FileWriter outFile = new FileWriter(args[1]);
+        FileWriter debugFile = new FileWriter(args[2]);
+    }
+}
